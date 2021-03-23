0ecf22d6e4fca227a09e6d54fae0faac901ce76c
Ben Clegg <bsclegg1@sheffield.ac.uk>
2021-03-23 14:30:35 +0000

    Tweaked solution inspector to also show execution output

--- src/main/java/tech/clegg/gradeer/preprocessing/SourceInspectorPreProcessor.java
+++ src/main/java/tech/clegg/gradeer/preprocessing/SourceInspectorPreProcessor.java
@@ -13,35 +13,70 @@ import java.util.Collection;
 
 public class SourceInspectorPreProcessor extends PreProcessor
 {
-    private final InspectionCommandProcess inspectionCommandProcess;
+    private final InspectorThread inspectorThread;
 
     public SourceInspectorPreProcessor(Solution solution, Configuration configuration)
     {
         super(solution, configuration);
-
-        // Load InspectionCommandProcess
-        Collection<Path> toInspect = new ArrayList<>();
-        // TODO find a more elegant solution for this
-        // method inside corresponding classes?
-        if(Files.exists(configuration.getTestOutputDir()))
-            toInspect.add(Paths.get(configuration.getTestOutputDir() + File.separator + solution.getIdentifier()).toAbsolutePath());
-        if(Files.exists(configuration.getMergedSolutionsDir()))
-            toInspect.add(Paths.get(configuration.getMergedSolutionsDir() + File.separator + solution.getIdentifier() + ".java").toAbsolutePath());
-        if(Files.exists(configuration.getSolutionCapturedOutputDir()))
-            toInspect.add(Paths.get(configuration.getSolutionCapturedOutputDir() + File.separator + solution.getIdentifier() + "-output").toAbsolutePath());
-
-        this.inspectionCommandProcess = new InspectionCommandProcess(configuration, toInspect);
+        inspectorThread = new InspectorThread(solution, configuration);
     }
 
     @Override
     public void start()
     {
-        inspectionCommandProcess.run();
+        inspectorThread.start();
     }
 
     @Override
-    public void stop()
+    public void stop() { inspectorThread.interrupt(); }
+
+    class InspectorThread extends Thread
     {
-        inspectionCommandProcess.stop();
+        private InspectionCommandProcess inspectionCommandProcess;
+        private final Solution solution;
+        private final Configuration configuration;
+
+        public InspectorThread(Solution solution, Configuration configuration)
+        {
+            this.solution = solution;
+            this.configuration = configuration;
+        }
+
+        @Override
+        public synchronized void start()
+        {
+            super.start();
+            // Wait before starting, need to let results get populated
+            // TODO replace with a wait/notify system
+            if(!configuration.getPreManualJavaClassesToExecute().isEmpty()) {
+                try {
+                    Thread.sleep(1000 * configuration.getPreManualJavaClassesToExecute().get(0).getWaitAfterExecutionTime());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Load InspectionCommandProcess
+            Collection<Path> toInspect = new ArrayList<>();
+            // TODO find a more elegant solution for this
+            // method inside corresponding classes?
+            if(Files.exists(configuration.getTestOutputDir()))
+                toInspect.add(Paths.get(configuration.getTestOutputDir() + File.separator + solution.getIdentifier()).toAbsolutePath());
+            if(Files.exists(configuration.getMergedSolutionsDir()))
+                toInspect.add(Paths.get(configuration.getMergedSolutionsDir() + File.separator + solution.getIdentifier() + ".java").toAbsolutePath());
+            if(Files.exists(configuration.getSolutionCapturedOutputDir()))
+                toInspect.add(Paths.get(configuration.getSolutionCapturedOutputDir() + File.separator + solution.getIdentifier() + "-output.txt").toAbsolutePath());
+
+            inspectionCommandProcess = new InspectionCommandProcess(configuration, toInspect);
+            inspectionCommandProcess.run();
+        }
+
+        @Override
+        public void interrupt()
+        {
+            super.interrupt();
+            if(inspectionCommandProcess != null)
+                inspectionCommandProcess.stop();
+        }
     }
 }
