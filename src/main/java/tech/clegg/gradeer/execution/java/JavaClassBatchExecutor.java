180106cfca1309237123016d24a7a4b55efcb420
Ben Clegg <ben@clegg.tech>
2022-09-19 16:29:08 +0100

    Reformat code

--- src/main/java/tech/clegg/gradeer/execution/java/JavaClassBatchExecutor.java
+++ src/main/java/tech/clegg/gradeer/execution/java/JavaClassBatchExecutor.java
@@ -10,38 +10,32 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class JavaClassBatchExecutor
-{
+public class JavaClassBatchExecutor {
     private final Solution solution;
     private final Configuration configuration;
     private final Collection<JavaExecutionManager> javaExecutionManagers;
 
-    public JavaClassBatchExecutor(Solution solution, Configuration configuration)
-    {
+    public JavaClassBatchExecutor(Solution solution, Configuration configuration) {
         this.solution = solution;
         this.configuration = configuration;
         this.javaExecutionManagers = new ArrayList<>();
         init();
     }
 
-    private void init()
-    {
+    private void init() {
         // Skip if no classes to execute
-        if(configuration.getPreManualJavaClassesToExecute().isEmpty())
+        if (configuration.getPreManualJavaClassesToExecute().isEmpty())
             return;
 
-        for (ClassExecutionTemplate cet : configuration.getPreManualJavaClassesToExecute())
-        {
+        for (ClassExecutionTemplate cet : configuration.getPreManualJavaClassesToExecute()) {
             javaExecutionManagers.add(new JavaExecutionManager(configuration, cet, solution));
         }
     }
 
-    public void runClasses()
-    {
+    public void runClasses() {
         System.out.println("Running classes for solution " + solution.getIdentifier());
 
-        if(javaExecutionManagers.isEmpty())
-        {
+        if (javaExecutionManagers.isEmpty()) {
             System.err.println("No classes marked for execution in pre-processing! Skipping...");
             return;
         }
@@ -50,24 +44,20 @@ public class JavaClassBatchExecutor
 
     }
 
-    public void stopExecutions()
-    {
+    public void stopExecutions() {
         System.out.println("Stopping executions of solution " + solution.getIdentifier());
 
-        if(javaExecutionManagers.isEmpty())
-        {
+        if (javaExecutionManagers.isEmpty()) {
             System.err.println("No classes marked for execution! Skipping...");
             return;
         }
 
-        for (JavaExecutionManager je : javaExecutionManagers)
-        {
+        for (JavaExecutionManager je : javaExecutionManagers) {
             je.stop();
         }
     }
 
-    private void storeCapturedOutput(List<String> capturedOutput)
-    {
+    private void storeCapturedOutput(List<String> capturedOutput) {
         DelayedFileWriter w = new DelayedFileWriter(capturedOutput);
         w.write(Paths.get(configuration.getSolutionCapturedOutputDir() +
                 File.separator + solution.getIdentifier() + "-output.txt"));
